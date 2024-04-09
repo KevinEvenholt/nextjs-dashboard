be909be97e24835cb8b3edcd1de48557b15c22c6
Kevin Evenholt <kevin@momang.io>
2024-04-09 20:09:31 +0200

    first comit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Customers</h1>
+      <p className="mt-2 text-gray-500">Here are your customers.</p>
+    </div>
+  );
+}
